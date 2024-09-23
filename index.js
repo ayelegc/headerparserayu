3cec699750a5087136406beea0a0769b1a8aebc1
Ayele Gobezie <ayele.gobezie@gmail.com>
2024-09-23 09:59:04 -0800

    Fix /api/whoami endpoint to pass IP,and endpoint at the

--- index.js
+++ index.js
@@ -6,8 +6,25 @@ require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// /api/whoami endpoint
-app.get('/api/whoami', (req, res) => {
+
+
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/whoami', function (req, res) {
+ 
   try {
     // Ensure proper IP address retrieval. req.ip handles proxy cases.
     const ipaddress = req.headers['x-forwarded-for'] || req.ip;
@@ -29,26 +46,6 @@ app.get('/api/whoami', (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 });
-
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
